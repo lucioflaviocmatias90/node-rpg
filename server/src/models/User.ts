7518401ac24c5406a55cbdecab7a449629a2ddaf
Lucio Flavio C Matias <lucioflavio.cmatias90@hotmail.com>
2020-12-14 21:52:10 -0300

    feat(server): setting typeorm package

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,24 +1,17 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import Hash from '../services/Hash'
+// import Hash from '../services/Hash'
 
-export interface IUser extends Document {
-  name: string,
-  email: string,
-  password: string,
-  gender: string,
-  birthday: Date,
+// UserSchema.pre('save', async function(this: IUser, next: Function) {
+//   this.password = await Hash.generate(this.password);
+// });
+
+import {Entity} from "typeorm";
+
+@Entity('users')
+export class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  gender: number;
+  birthday: Date;
 }
-
-const UserSchema: Schema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  gender: String,
-  birthday: Date,
-});
-
-UserSchema.pre('save', async function(this: IUser, next: Function) {
-  this.password = await Hash.generate(this.password);
-});
-
-export default mongoose.model<IUser>('User', UserSchema);
