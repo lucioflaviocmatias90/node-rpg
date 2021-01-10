140780a2d2ef7cf6a9e564c71d3c9a569bf84066
Lucio Flavio C Matias <lucioflavio.cmatias90@hotmail.com>
2021-01-10 13:24:57 -0300

    test(server): creating tests to session

--- server/src/tests/sessions.test.ts
+++ server/src/tests/sessions.test.ts
@@ -28,16 +28,7 @@ beforeEach(async () => {
 
 describe('POST /sessions', () => {
   it('should to create a new session', async () => {
-    const userRepository = database.connection.getRepository(User);
-    const user = userRepository.create({
-      name: userData.name,
-      password: '123123',
-      email: userData.email,
-      gender: userData.gender,
-      birthday: userData.birthday
-    });
-
-    const newUser = await userRepository.save(user);
+    const newUser = await createUser();
 
     const response = await request(app).post('/sessions').send({
       email: userData.email,
@@ -48,4 +39,46 @@ describe('POST /sessions', () => {
 
     expect(userId).toBe(newUser.id);
   });
+
+  it('should return error when send wrong password', async () => {
+    const newUser = await createUser();
+
+    const response = await request(app).post('/sessions').send({
+      email: newUser.email,
+      password: '123456'
+    });
+
+    const { error } = response.body;
+
+    expect(error.code).toBe('002');
+    expect(error.message).toBe('Email ou senha inválido');
+  });
+
+  it('should return error when send non-existing email', async () => {
+    await createUser();
+
+    const response = await request(app).post('/sessions').send({
+      email: 'non_existing_email@email.com',
+      password: '123123'
+    });
+
+    const { error } = response.body;
+
+    expect(error.code).toBe('001');
+    expect(error.message).toBe('Email ou senha inválido');
+  });
 });
+
+const createUser = async () => {
+  const userRepository = database.connection.getRepository(User);
+
+  const user = userRepository.create({
+    name: userData.name,
+    password: '123123',
+    email: userData.email,
+    gender: userData.gender,
+    birthday: userData.birthday
+  });
+
+  return await userRepository.save(user);
+};
